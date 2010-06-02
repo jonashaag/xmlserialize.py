c6293b1fc6a8222da3d6132b4f5d5d736a54b569
Jonas Haag <jonas@lophus.org>
2010-06-02 21:56:54 +0200

    mv  stuff/; +setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name        = 'xmlserialize.py',
+    version     = '1',
+    description = 'An extensible Python-(to|from)-xml-(un)serializer in beautiful pure-Python code',
+    author      = 'Jonas Haag',
+    author_email= 'jonas@lophus.org',
+    url         = 'http://github.com/jonashaag/xmlserialize.py',
+    license     = '2-clause BSD',
+    py_modules  = ['xmlserialize']
+)
